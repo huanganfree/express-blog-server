24af78e9f76e6c1464d3d7ad0280d166d9d4a303
huanganfree <huangan1206@163.com>
2023-05-31 20:24:53 +0800

    更改数据库host

--- db/db.config.js
+++ db/db.config.js
@@ -1,8 +1,8 @@
 // 创建mysql连接，这里连接到phpamdin的数据库
 module.exports = {
-  host: 'http://localhost', // 服务器地址
+  host: '114.55.238.107', // 服务器地址 本地：localhost  生产：114.55.238.107
   port: '3306', // 端口
   database: 'test', // 数据库名称
   user: 'root', // mysql用户名称
-  password: 'rootroot123', // mysql用户密码 本地开发：root123456 ，生产：rootroot123
+  password: 'root123456', // mysql用户密码 本地开发：root123456 ，生产：rootroot123
 }

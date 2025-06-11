f442f2cc110450b0a197e65782842e683aa75b83
huanganfree <huangan1206@163.com>
2025-06-11 22:38:23 +0800

    feat: 完成分页查询

--- routers/about.js
+++ routers/about.js
@@ -6,14 +6,22 @@ router.use((req, res, next) => {
   next()
 })
 
-router.get('/about', function(req, res) {
+
+router.get('/about', async function(req, res) {
   console.log(req.session.userId);
-  dbQueryPromise(`SELECT * FROM user_profile WHERE user_id='${req.session.userId}'`) //mysql中间件无法识别传入参数
+  const total = await dbQueryPromise(`SELECT * FROM user_profile WHERE user_id='${req.session.userId}'`)
+  const offset = (+req.query.pageSize) * (req.query.pageNum - 1)
+  const limit = +req.query.pageSize
+  // 分页查询
+  dbQueryPromise(`SELECT * FROM user_profile WHERE user_id='${req.session.userId}' ORDER BY id LIMIT ${limit} OFFSET ${offset}`) //mysql中间件无法识别传入参数
     .then((results) => {
       res.json({
         code: 200,
         message: '成功',
-        data: results
+        data: {
+          records: results || [],
+          total: total.length || 0,
+        }
       })
     })
     .catch(err => {

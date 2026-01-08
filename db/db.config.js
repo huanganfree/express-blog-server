5436d85529cd2a76ba337ab44d1ec86852b403c9
huanganfree <huangan1206@163.com>
2026-01-08 23:31:30 +0800

    feat: pm2配置文件功能完成，打印日志加时间格式

--- db/db.config.js
+++ db/db.config.js
@@ -1,9 +1,10 @@
 // 连接到本地的mysql数据库
 // 这里的配置是为了连接到本地的mysql数据库
+
 module.exports = {
   host: 'localhost', // 生产和开发环境都是该地址
   port: '3306',
   database: 'express_blog_server',
-  user: 'root',
-  password: 'rootHA123@',
+  user: process.env.databaseUser,
+  password: process.env.databasePassword,
 }
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,26 @@
 module.exports = {
-  apps: [{
-    name: "app",
-    script: "./bin/www.js",
-    watch: true,
-    env_development: {
-      "baseURL": "localhost",
-      "port": 8000,
-      "NODE_ENV": "development",
-      "databaseUser": "root",
-      "databasePassword": "rootHA@",
+  apps: [
+    {
+      name: "app",
+      script: "./bin/www.js",
+      watch: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss', // 日志时间
+      env_development: {
+        // 开发环境
+        baseURL: "localhost",
+        port: 8000,
+        NODE_ENV: "development",
+        databaseUser: "root",
+        databasePassword: "rootHA@",
+      },
+      env_production: {
+        // 生产环境
+        baseURL: "120.26.94.24",
+        port: 81,
+        NODE_ENV: "production",
+        databaseUser: "root",
+        databasePassword: "rootHA123@",
+      },
     },
-    env_production: {
-      "baseURL": "120.26.94.24",
-      "port": 81,
-      "NODE_ENV": "production",
-      "databaseUser": "root",
-      "databasePassword": "rootHA123@",
-    }
-  }]
-}
+  ],
+};
